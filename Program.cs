4607f0eddae7de0318c962ab0cf75ce1b2567358
BrookeAL001 <86929564+BrookeAL001@users.noreply.github.com>
2022-06-08 14:46:20 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassExerciseCars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Truck = new Car();
+            Truck.Make = "Toyota";
+            Truck.Model = "Tundra";
+            Truck.Year = 2020;
+
+            var cars = new List<Car>();
+            cars.Add(Truck);
+
+            foreach (var Car in cars)
+            {
+                Console.WriteLine($"{Car.Make} {Car.Model} {Car.Year}");
+            }
+        }
+
+    }
+}
